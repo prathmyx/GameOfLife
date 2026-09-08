c20fd7ecb259b0ba6055f6fbf82f6ff90f0f5092
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-08 18:53:33 +0530

    feat:added cell rendering

--- script/main.js
+++ script/main.js
@@ -1,11 +1,12 @@
 import {ROWS, COLS, SIZE} from "./config.js"
-import { initBoard, drawBoard } from "./board.js"
+import { initBoard, drawBoard, getAdjacentCount } from "./board.js"
 
 document.documentElement.style.setProperty('--rows', ROWS);
 document.documentElement.style.setProperty('--cols', COLS);
 document.documentElement.style.setProperty('--size', `${SIZE}px`);
 
-const canvas = document.getElementById('main-board');
+const board = initBoard()
 
-drawBoard(initBoard(ROWS, COLS), canvas);
+
+drawBoard(board);
 
