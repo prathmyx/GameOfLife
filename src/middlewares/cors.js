5a093e769799d7ef1564c6d264979f9871f6e7e6
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-10 14:43:16 +0530

    fix:cors error

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,9 +1,12 @@
 function setCORS(req, res, next) {
     const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://127.0.0.1:3000';
-    const origins = [CLIENT_ORIGIN, 'https://prathmyx.github.io'];
+    const allowedOrigins = [CLIENT_ORIGIN, 'https://prathmyx.github.io'];
 
-    res.setHeader('Access-Control-Allow-Origin', origins);
+    if (allowedOrigins.includes(req.headers.origin)) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    }
 
+    res.setHeader("Vary", "Origin");
     next();
 }
 
