4b3c32ae28b5c50b8bba7e47b2093a65725b555a
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-10 13:13:46 +0530

    fix: added github domain on cors origin

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,8 @@
 function setCORS(req, res, next) {
     const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://127.0.0.1:3000';
-    res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN);
+    const origins = [CLIENT_ORIGIN, 'https://prathmyx.github.io'];
+
+    res.setHeader('Access-Control-Allow-Origin', origins);
 
     next();
 }
