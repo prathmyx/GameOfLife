b5de54ad0c275d4654cc691e8046f905f39c3ef7
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-11 11:44:08 +0530

    feat:added signup form

--- script/handleAuth.js
+++ script/handleAuth.js
@@ -3,6 +3,8 @@ import { SERVER_URL } from "./config.js";
 const loginForm = document.getElementById('loginForm');
 const signupForm = document.getElementById('signupForm');
 
+const statusEl = document.getElementById('auth-status');
+
 export function handleAuthClick() {
     loginForm.addEventListener('submit', handleLogin);
     signupForm.addEventListener('submit', handleSign);
@@ -24,18 +26,46 @@ async function handleLogin(e) {
 
         const data = await response.json();
         
-        console.log(data);
         if (!response.ok) {
-            throw new Error(data.message);
+            throw new Error(data.error);
         }
 
         console.log('Authentication Succesfull');
     } catch (err) {
-        console.log(err.message);
+        console.log(err);
+        statusEl.textContent = err.message;
     }
 }
 
-function handleSign() {
+async function handleSign(e) {
+    e.preventDefault();
+    try {
+        const username = signupForm.elements["username"].value;
+        const password = signupForm.elements["password"].value;
+        const cpassword = signupForm.elements["cpassword"].value;
 
+        if (password != cpassword) {
+            throw new Error("Password and Confirm Password are not same!");
+        }
+
+        const response = await fetch(`${SERVER_URL}/auth/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        });
+
+        const data = await response.json();
+        
+        if (!response.ok) {
+            throw new Error(data.error);
+        }
+
+        console.log('Authentication Succesfull');
+    } catch (err) {
+        console.log(err);
+        statusEl.textContent = err.message;
+    }
 }
 
