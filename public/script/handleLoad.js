7b4bd456112381ce5012411adaeccadd44f79871
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-11 13:39:01 +0530

    feat:added load and save

--- public/script/handleLoad.js
+++ public/script/handleLoad.js
@@ -1,5 +1,19 @@
 import { SERVER_URL } from "./config.js";
 
-export function handleLoad() {
-    
+export async function handleLoad() {
+    try {
+        let username = 'abc';
+        const response = await fetch(`${SERVER_URL}/api/v1/users/${username}/games`);
+
+        const data = await response.json();
+        
+        if (!response.ok) {
+            throw new Error(data.error);
+        }
+
+        console.log('Loaded Succesfully', data);
+    } catch (err) {
+        console.log(err);
+        statusEl.textContent = err.message;
+    }
 }
