bc39cba75ea4384bf3a42a29ef932f5b4cfb86a4
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-11 13:39:01 +0530

    feat:added load and save

--- script/handleSave.js
+++ script/handleSave.js
@@ -1,5 +1,26 @@
 import { SERVER_URL } from "./config.js";
+import { Game } from "./main.js";
 
-export function handleSave() {
-    
+export async function handleSave() {
+    try {
+        let username = 'abc';
+        const response = await fetch(`${SERVER_URL}/api/v1/users/${username}/games`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({grid: Game.board})
+        });
+
+        const data = await response.json();
+        
+        if (!response.ok) {
+            throw new Error(data.error);
+        }
+
+        console.log('Saved Succesfully');
+    } catch (err) {
+        console.log(err);
+        statusEl.textContent = err.message;
+    }
 }
