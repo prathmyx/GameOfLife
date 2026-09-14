a331d3c0135d23956001b8bf55d20805cf73e4af
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-14 12:12:29 +0530

    feat:added localstorage to save state of log

--- script/main.js
+++ script/main.js
@@ -6,6 +6,17 @@ import { handleAuthClick } from "./handleAuth.js";
 document.documentElement.style.setProperty('--rows', ROWS);
 document.documentElement.style.setProperty('--cols', COLS);
 
+
+const authBtn = document.getElementById("authBtn");
+const usernameEl = document.getElementById('username-display');
+
+if (!localStorage.getItem('logged')) {
+    localStorage.setItem('logged', 'false');
+} else if (JSON.parse(localStorage.getItem('logged'))) {
+    authBtn.textContent = "Logout";
+    usernameEl.textContent = `Hi, ${localStorage.getItem('username')}`;
+}
+
 export const Game = {
     population: 0,
     board: initBoard(),
