295e1cb8e476c3e6efc59aaae9e9965c0bcd6a2e
prathmyx <pratham.yadav.gr8@outlook.com>
2026-09-14 12:42:09 +0530

    custom dev script

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,6 @@
 function setCORS(req, res, next) {
     const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://127.0.0.1:3000';
-    const allowedOrigins = [CLIENT_ORIGIN, 'https://prathmyx.github.io'];
+    const allowedOrigins = [CLIENT_ORIGIN, 'http://127.0.0.1:3000'];
 
     if (allowedOrigins.includes(req.headers.origin)) {
         res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
